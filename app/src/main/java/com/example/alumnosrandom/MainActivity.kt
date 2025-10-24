2274e2fb76b1a39615206a1296b1fd72786f9a7b
davberamo <dberlinches2003@gmail.com>
2025-10-24 19:52:18 +0200

    Defino los estados para el alumno actual y para la lista de sorteados

--- app/src/main/java/com/example/alumnosrandom/MainActivity.kt
+++ app/src/main/java/com/example/alumnosrandom/MainActivity.kt
@@ -1,18 +1,30 @@
 package com.example.alumnosrandom
 
+import android.R
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.alumnosrandom.ui.theme.AlumnosRandomTheme
 import kotlin.random.Random
 
@@ -29,12 +41,13 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun RandomAlumnos() {
-    val nombresAlumnos = listOf("David Berlinches", "Carlos Barrera", "Iker Toribio", "Benjamin Vargas", "David Salvador", "Daniel Beltrán")
+fun RandomAlumnos(modifier: Modifier = Modifier
+    .fillMaxSize()
+    .wrapContentSize(Alignment.Center)) {
+    val nombresAlumnos = listOf("David Berlinches", "Carlos Barrera", "Iker Toribio", "Benjamin Vargas", "David Salvador", "Daniel Beltrán", "Abzael Rodriguez")
 
 
     val alumnos = remember {
-        val alumnosAleatorios = nombresAlumnos.random()
         val numerosAleatorios = (1..21).shuffled()
         val listaAlumnos = mutableListOf<Alumno>()
         for (i in nombresAlumnos.indices) {
@@ -43,14 +56,17 @@ fun RandomAlumnos() {
             listaAlumnos.add(Alumno(nombre, numero))
         }
         listaAlumnos
-
     }
+    val alumnosSorteados = remember { mutableStateListOf<Alumno>() }
+    var alumnoActual by remember { mutableStateOf<Alumno?>(null) }
+
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     AlumnosRandomTheme {
-
+        RandomAlumnos()
     }
 }
